2861259401fd9785ad6de505dee22f3ee9e12d6f
Aman Song <amansong70@gmail.com>
2024-11-26 21:03:25 +0000

    deploy once more

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 test('renders the main page (Main component) on /', () => {
   render(<App />);
 
-  const mainElement = screen.getByText(/Wen Ting Song/i); 
-  expect(mainElement).toBeInTheDocument();
+  const matchingElements = screen.getAllByText(/Wen Ting Song/i);
+
+  expect(matchingElements[0]).toBeInTheDocument();
 });
