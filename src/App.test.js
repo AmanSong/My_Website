913522d01c7878bfc3e5ba6841bd999473fa1408
Aman Song <amansong70@gmail.com>
2024-11-26 20:57:54 +0000

    deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders the main page (Main component) on /', () => {
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>
-  );
+  render(<App />);
 
-  // Check for something specific that Main renders
-  const mainElement = screen.getByText(/Wen Ting Song/i); // Replace with actual content from Main
+  const mainElement = screen.getByText(/Wen Ting Song/i); 
   expect(mainElement).toBeInTheDocument();
 });
